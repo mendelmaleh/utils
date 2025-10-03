f637ad60569993be3ec6fb8874019d21f01c8f04
Mendel Elmaleh <mendel@elmaleh.it>
2025-10-03 04:20:26 +0200

    Initial commit

---
+++ dates/dates.go
@@ -0,0 +1,23 @@
+package dates
+
+import "time"
+
+const ISO8601 = "2006-01-02"
+
+// t.Format(dates.ISO8601)
+// t.Format("2006-01-02")
+// t.Format(ISO8601)
+// dates.ISO8601(t)
+// dates.Fmt(t)
+// fmtdate(t)
+
+func Fmt(t time.Time) string {
+	return t.Format(ISO8601)
+}
+
+// utils.Date
+// dates.New
+
+func New(year int, month time.Month, day int) time.Time {
+	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+}
